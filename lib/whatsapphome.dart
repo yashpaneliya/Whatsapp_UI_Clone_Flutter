549014c3556dece55297f8948661cbd39e233b5a
YASH <yashsp1158@gmail.com>
2020-04-11 12:17:21 +0530

    Home Page Updated

--- lib/whatsapphome.dart
+++ lib/whatsapphome.dart
@@ -5,11 +5,49 @@ class WhatsAppHome extends StatefulWidget {
   _WhatsAppHomeState createState() => _WhatsAppHomeState();
 }
 
-class _WhatsAppHomeState extends State<WhatsAppHome> {
+class _WhatsAppHomeState extends State<WhatsAppHome> with SingleTickerProviderStateMixin{
+
+  TabController tbc;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    tbc=TabController(length: 4, vsync: this,initialIndex: 1);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WhatsApp'),
+        elevation: 0.5,
+        bottom: TabBar(
+          controller: tbc,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            Tab(
+              icon: Icon(Icons.camera_alt),
+            ),
+            Tab(child: Text('Chats')),
+            Tab(child: Text('Status')),
+            Tab(child: Text('Calls')),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: tbc,
+        children: <Widget>[
+          CameraPage(),
+          ChatPage(),
+          StatusPage(),
+          CallPage()
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).accentColor,
+        child: Icon(Icons.message),
+        onPressed: (){},
+      ),
     );
   }
 }
