9ad819aaf57e9063f34b34671c71409688b23fa5
YASH <yashsp1158@gmail.com>
2020-04-11 19:58:19 +0530

    Status model added

--- lib/models/chatmodel.dart
+++ lib/models/chatmodel.dart
@@ -5,11 +5,11 @@ class ChatModel {
   final String image;
 
   ChatModel(this.name, this.msg, this.time, this.image);
-  
-  List<ChatModel> temp = [
-    ChatModel('Yash Paneliya', 'Heyyy There', '07:14 PM', 'https://images.unsplash.com/photo-1466112928291-0903b80a9466?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80'),
+}
+
+List<ChatModel> temp = [
+    ChatModel('Sundar Pichai', 'Heyyy Yash', '07:14 PM', 'https://images.unsplash.com/photo-1466112928291-0903b80a9466?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80'),
     ChatModel('Octrus','Hello World','05:11 PM','https://images.unsplash.com/photo-1466112928291-0903b80a9466?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80'),
     ChatModel('FlutterDev', 'Heyy GDE Flutter!!!', '07:14 PM', 'https://images.unsplash.com/photo-1466112928291-0903b80a9466?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80'),
     ChatModel('Dart','Hello GDE Dart!!!','05:11 PM','https://images.unsplash.com/photo-1466112928291-0903b80a9466?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80')
   ];
-}
---
+++ lib/models/statusmodel.dart
@@ -0,0 +1,18 @@
+class StatusModel{
+  final String name;
+  final String time;
+  final String imgPath;
+
+  StatusModel(this.name, this.time, this.imgPath);
+}
+
+List<StatusModel> stdata=[
+StatusModel('Sundar Pichai','Today, 6:07 PM','https://images.unsplash.com/photo-1466112928291-0903b80a9466?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80'),
+StatusModel('FlutterDev','Today, 5:07 PM','https://images.unsplash.com/photo-1466112928291-0903b80a9466?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80'),
+StatusModel('Octrus','Today, 10:23 PM','https://images.unsplash.com/photo-1466112928291-0903b80a9466?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80'),
+StatusModel('Dart','Today, 8:07 AM','https://images.unsplash.com/photo-1466112928291-0903b80a9466?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80'),
+StatusModel('Sundar Pichai','Today, 6:07 PM','https://images.unsplash.com/photo-1466112928291-0903b80a9466?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80'),
+StatusModel('FlutterDev','Today, 5:07 PM','https://images.unsplash.com/photo-1466112928291-0903b80a9466?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80'),
+StatusModel('Octrus','Today, 10:23 PM','https://images.unsplash.com/photo-1466112928291-0903b80a9466?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80'),
+StatusModel('Dart','Today, 8:07 AM','https://images.unsplash.com/photo-1466112928291-0903b80a9466?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80'),
+];
